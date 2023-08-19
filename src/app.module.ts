705cc6854c32fef81aff1998b79a4834895e6fc8
Pedro Andres Gasca Manrique <pedroandres.gasca.contractor@bbva.com>
2023-08-18 22:45:20 -0600

    Develop branch is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, BrandsModule],
   controllers: [AppController],
   providers: [AppService],
 })
